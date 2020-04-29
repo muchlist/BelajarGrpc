6e01d2296032a57098ca82083bea571f537bb914
mrtsdc <whois.muchlis@gmail.com>
2020-04-29 12:31:04 +0800

    implement bidi streaming in calculator client

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/muchlist/BelajarGrpc/calculator/calculatorpb"
 
@@ -23,7 +24,8 @@ func main() {
 
 	//doUnary(c)
 	// doServerStreaming(c)
-	doClientStreaming(c)
+	//doClientStreaming(c)
+	doBiDiStreaming(c)
 }
 
 func doUnary(c calculatorpb.CalculatorServiceClient) {
@@ -83,4 +85,47 @@ func doClientStreaming(c calculatorpb.CalculatorServiceClient) {
 	}
 
 	fmt.Printf("The Average is: %v", res.GetAverage())
+}
+
+func doBiDiStreaming(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do Maximum bidi Streaming")
+
+	stream, err := c.FindMaximum(context.Background())
+	if err != nil {
+		log.Fatalf("Error while opening stream and calling findMaximum: %v", err)
+	}
+
+	waitc := make(chan struct{})
+
+	//send goroutine
+	go func() {
+		numbers := []int32{4, 7, 19, 4, 32, 5, 3}
+		for _, number := range numbers {
+			fmt.Printf("Sending number : %v\n", number)
+			stream.Send(&calculatorpb.FindMaximumRequest{
+				Number: number,
+			})
+			time.Sleep(1000 * time.Millisecond)
+		}
+		stream.CloseSend()
+	}()
+
+	//receive goroutine
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Problem while reading server stream : %v\n", err)
+				break
+			}
+			maximum := res.GetMaximum()
+			fmt.Printf("Received a new maximum of... : %v\n", maximum)
+		}
+		close(waitc)
+	}()
+
+	<-waitc
 }
